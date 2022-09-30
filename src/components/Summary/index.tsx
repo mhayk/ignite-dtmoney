dd3cb5390f817843e3c92517bad5062d6be06d0b
Mhayk Whandson <hi@mhayk.com>
2022-09-30 16:13:03 +0100

    feat: add summary components and its styles

---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,33 @@
+import { SummaryCard, SummaryContainer } from "./styles";
+import { ArrowCircleUp, ArrowCircleDown, CurrencyDollar } from 'phosphor-react'
+
+export function Summary() {
+    return (
+        <SummaryContainer>
+            <SummaryCard>
+                <header>
+                    <span>Incomes</span>
+                    <ArrowCircleUp size={32} color="#00b37e" />
+                </header>
+
+                <strong>£ 17,400.00</strong>
+            </SummaryCard>
+            <SummaryCard>
+                <header>
+                    <span>Outcomes</span>
+                    <ArrowCircleDown size={32} color="#f75a68" />
+                </header>
+
+                <strong>£ 17,400.00</strong>
+            </SummaryCard>
+            <SummaryCard variant="green">
+                <header>
+                    <span>Total</span>
+                    <CurrencyDollar size={32} color="#fff" />
+                </header>
+
+                <strong>£ 17,400.00</strong>
+            </SummaryCard>
+        </SummaryContainer>
+    )
+}
