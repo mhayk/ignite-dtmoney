bebe4565e9ece0415b675e7d885b8ef75d3d9722
Mhayk Whandson <hi@mhayk.com>
2022-09-30 12:04:21 +0100

    style:

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -20,6 +20,6 @@ export const GlobalStyle = createGlobalStyle`
     }
 
     body, input-security, textarea, button {
-        font: 400 1rem 'Roboto, sans-serif';
+        font: 400 1rem Roboto, sans-serif;
     }
 `;
