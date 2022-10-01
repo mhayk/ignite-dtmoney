50939032d8a38595169c4ca7e3aeee8c234d7bd5
Mhayk Whandson <hi@mhayk.com>
2022-10-01 12:36:28 +0100

    feat: create table to list all transactions

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,59 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
 
 export function Transactions() {
     return (
         <div>
             <Header />
             <Summary />
+
+            <TransactionsContainer>
+                <TransactionsTable>
+                    <tbody>
+                        <tr>
+                            <td width="50%">Website development</td>
+                            <td>
+                                <PriceHighlight variant="income">
+                                    £ 12,000.00
+                                </PriceHighlight>
+                            </td>
+                            <td>Sell</td>
+                            <td>18/04/2022</td>
+                        </tr>
+                        <tr>
+                            <td width="50%">Website development</td>
+                            <td>
+                                <PriceHighlight variant="income">
+                                    £ 12,000.00
+                                </PriceHighlight>
+                            </td>
+                            <td>Sell</td>
+                            <td>18/04/2022</td>
+                        </tr>
+                        <tr>
+                            <td width="50%">Hamburger</td>
+                            <td>
+                                <PriceHighlight variant="outcome">
+                                    - £ 9.00
+                                </PriceHighlight>
+                            </td>
+                            <td>Food</td>
+                            <td>29/09/2022</td>
+                        </tr>
+                        <tr>
+                            <td width="50%">Website development</td>
+                            <td>
+                                <PriceHighlight variant="income">
+                                    £ 12,000.00
+                                </PriceHighlight>
+                            </td>
+                            <td>Sell</td>
+                            <td>18/04/2022</td>
+                        </tr>
+                    </tbody>
+                </TransactionsTable>
+            </TransactionsContainer>
         </div>
     )
 }
