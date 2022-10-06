15cbe67679c7f5d0f3fa5aac6db51c62abeaf89e
Mhayk Whandson <hi@mhayk.com>
2022-10-06 16:14:08 +0100

    feat: add styles for transaction modal component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,6 +2,7 @@ import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
 import * as Dialog from '@radix-ui/react-dialog'
 
 import logoImg from '../../assets/logo.svg'
+import { NewTransactionModal } from "../NewTransactionModal";
 
 export function Header() {
     return (
@@ -13,15 +14,7 @@ export function Header() {
                     <Dialog.Trigger asChild>
                         <NewTransactionButton>New transaction</NewTransactionButton>
                     </Dialog.Trigger>
-
-                    <Dialog.Portal>
-                        <Dialog.Overlay />
-
-                        <Dialog.Content>
-                            <Dialog.Title>New transaction</Dialog.Title>
-                            <Dialog.Close />
-                        </Dialog.Content>
-                    </Dialog.Portal>
+                    <NewTransactionModal />
                 </Dialog.Root>
             </HeaderContent>
 
