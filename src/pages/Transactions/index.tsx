4149ffbd5766686a51742771cd700ef11064ce6b
Mhayk Whandson <hi@mhayk.com>
2022-10-08 16:08:32 +0100

    feat: create the request of the transactions from the back-end

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,9 +1,31 @@
+import { useEffect, useState } from "react";
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
 import { SearchForm } from "./components/SearchForm";
 import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
 
+interface Transaction {
+    id: number;
+    description: string;
+    type: "income" | "outcome";
+    price: number;
+    category: string;
+    createdAt: string;
+}
+
 export function Transactions() {
+    const [transactions, setTransactions] = useState<Transaction[]>([])
+    async function loadTransactions() {
+        const response = await fetch('http://localhost:3333/transactions')
+        const data = await response.json()
+
+        setTransactions(data)
+    }
+
+    useEffect(() => {
+        loadTransactions()
+    }, [])
+
     return (
         <div>
             <Header />
@@ -13,46 +35,20 @@ export function Transactions() {
                 <SearchForm />
                 <TransactionsTable>
                     <tbody>
-                        <tr>
-                            <td width="50%">Website development</td>
-                            <td>
-                                <PriceHighlight variant="income">
-                                    £ 12,000.00
-                                </PriceHighlight>
-                            </td>
-                            <td>Sell</td>
-                            <td>18/04/2022</td>
-                        </tr>
-                        <tr>
-                            <td width="50%">Website development</td>
-                            <td>
-                                <PriceHighlight variant="income">
-                                    £ 12,000.00
-                                </PriceHighlight>
-                            </td>
-                            <td>Sell</td>
-                            <td>18/04/2022</td>
-                        </tr>
-                        <tr>
-                            <td width="50%">Hamburger</td>
-                            <td>
-                                <PriceHighlight variant="outcome">
-                                    - £ 9.00
-                                </PriceHighlight>
-                            </td>
-                            <td>Food</td>
-                            <td>29/09/2022</td>
-                        </tr>
-                        <tr>
-                            <td width="50%">Website development</td>
-                            <td>
-                                <PriceHighlight variant="income">
-                                    £ 12,000.00
-                                </PriceHighlight>
-                            </td>
-                            <td>Sell</td>
-                            <td>18/04/2022</td>
-                        </tr>
+                        {transactions.map(transaction => {
+                            return (
+                                <tr key={transaction.id}>
+                                    <td width="50%">{transaction.description}</td>
+                                    <td>
+                                        <PriceHighlight variant={transaction.type}>
+                                            {transaction.price}
+                                        </PriceHighlight>
+                                    </td>
+                                    <td>{transaction.category}</td>
+                                    <td>{transaction.createdAt}</td>
+                                </tr>
+                            )
+                        })}
                     </tbody>
                 </TransactionsTable>
             </TransactionsContainer>
