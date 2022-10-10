4ce769b77f6d1c0ddee1e1fe3f9c6f0ee3c6b940
Mhayk Whandson <hi@mhayk.com>
2022-10-10 12:00:18 +0100

    feat: add function to create a new transaction via context api

--- src/contexts/TransactionsContext.tsx
+++ src/contexts/TransactionsContext.tsx
@@ -13,10 +13,18 @@ interface Transaction {
 interface TransactionContextType {
     transactions: Transaction[];
     fetchTransactions: (query?: string) => Promise<void>;
+    createTransaction: (data: CreateTransactionInput) => Promise<void>;
 }
 
 interface TransactionsProviderProps {
     children: ReactNode
+}
+
+interface CreateTransactionInput {
+    description: string;
+    price: number;
+    category: string;
+    type: 'income' | 'outcome';
 }
 
 export const TransactionsContext = createContext({} as TransactionContextType)
@@ -36,11 +44,27 @@ export function TransactionsProvider({ children }: TransactionsProviderProps) {
         */
         const response = await api.get('transactions', {
             params: {
-                q: query
+                q: query,
+                _sort: 'createdAt',
+                _order: 'desc'
             }
         })
 
         setTransactions(response.data)
+    }
+
+    async function createTransaction(data: CreateTransactionInput) {
+        const { description, price, category, type } = data;
+
+        const response = await api.post('transactions', {
+            description,
+            price,
+            category,
+            type,
+            createdAt: new Date()
+        })
+
+        setTransactions(state => [response.data, ...state])
     }
 
     useEffect(() => {
@@ -49,7 +73,8 @@ export function TransactionsProvider({ children }: TransactionsProviderProps) {
     return (
         <TransactionsContext.Provider value={{
             transactions,
-            fetchTransactions
+            fetchTransactions,
+            createTransaction
         }}>
             {children}
         </TransactionsContext.Provider>
